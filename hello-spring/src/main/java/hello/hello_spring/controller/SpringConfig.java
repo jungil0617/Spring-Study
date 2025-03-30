46db4ba5623820a927dabe0d4f01d34799163e5e
jungil0617 <jungil0617@naver.com>
2025-03-30 23:20:49 +0900

    [#1] 스프링 빈과 의존관계

---
+++ hello-spring/src/main/java/hello/hello_spring/controller/SpringConfig.java
@@ -0,0 +1,20 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+}
