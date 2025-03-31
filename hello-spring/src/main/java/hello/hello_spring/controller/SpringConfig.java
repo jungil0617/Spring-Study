6c87e8caa5c11625a4b85d2df0639354bebfc1cc
jungil0617 <jungil0617@naver.com>
2025-03-31 23:32:56 +0900

    [#1] H2, JDBC

--- hello-spring/src/main/java/hello/hello_spring/controller/SpringConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/SpringConfig.java
@@ -1,11 +1,23 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.repository.JdbcTemplateMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
+@Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -14,7 +26,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+     // return new MemoryMemberRepository();
+     // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
